091314be8870a56d8acb18f3dea134d316006652
wangthong <wangthong88899@gmail.com>
2021-02-25 18:44:00 +0700

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SearchbarComponent } from './home/navbar/searchbar/searchbar.component';
+import { CategoryComponent } from './home/category/category.component';
+import { TopbannerComponent } from './home/topbanner/topbanner.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SearchbarComponent,
+    CategoryComponent,
+    TopbannerComponent
   ],
   imports: [
     BrowserModule
